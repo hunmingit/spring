c9f317c9f93121e83ad2b0f2b3c5c53da9c6be96
hunmingit <pandore470470@gmail.com>
2026-01-20 17:43:11 +0900

    HomeController.java 업데이트

--- chapter04_MVC/src/main/java/org/joonzis/controller/HomeController.java
+++ chapter04_MVC/src/main/java/org/joonzis/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page. 기본 화면 이동
+ * Handles requests for the application home page. 기본 화면 이동하기
  */
 @Controller
 public class HomeController {
